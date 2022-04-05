8b2c0e9552e90feed1282e3a4ce79854b0a2b5fe
Ankur357 <ankur.gautam_cs20@gla.ac.in>
2022-04-05 19:19:47 +0530

    reverse string

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -1,15 +1,41 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 public class ReverseString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        String rstr = "";
-        char ch;
+//        String rstr = "";
+//        char ch;
+//        for (int i = 0; i < str.length(); i++) {
+//            ch = str.charAt(i);
+//            rstr = ch+rstr;
+//        }
+//        System.out.println(rstr);
+
+        //USing String Builder
+//        StringBuilder rstr = new StringBuilder();
+//        rstr.append(str);
+//        rstr.reverse();
+//        System.out.println(rstr);
+
+        // Using toCharArray method
+//        char str1[] = str.toCharArray();
+//        for (int i = str1.length-1; i >=0; i--) {
+//            System.out.print(str1[i]);
+
+        // Using Stack
+        char str1[] = new char[str.length()];
+        Stack<Character> s = new Stack<Character>();
         for (int i = 0; i < str.length(); i++) {
-            ch = str.charAt(i);
-            rstr = ch+rstr;
+            s.push(str.charAt(i));
         }
-        System.out.println(rstr);
+        int i=0;
+        while (!s.isEmpty()){
+            str1[i++] = s.pop();
+        }
+        for (int j = 0; j < str1.length; j++) {
+            System.out.print(str1[j]);
+        }
     }
 }
